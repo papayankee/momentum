99c912e3ff62ac2d4056dc643c51a8027fbe72b2
pyglee <realmanpaul@gmail.com>
2023-04-17 14:52:02 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+
+const h1 = document.querySelector("div.hello:first-child h1")
+
+
+function handleTitleClick() {
+    const clickedClass = "clicked";
+    if (h1.classList.contains(clickedClass)) {
+      h1.classList.remove(clickedClass);
+    } else {
+      h1.classList.add(clickedClass);
+    }
+    console.log("handleTitleClick function was executed!")
+  }
+
+function handleTitleClick() {
+    h1.classList.toggle("clicked");
+  }
+  
+
+
+h1.addEventListener("click", handleTitleClick);
+
+// Path: app.js
+
