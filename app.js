5da8f492b770f6386c5a1f538261ac384470fddb
pyglee <realmanpaul@gmail.com>
2023-04-23 04:31:23 +0900

    4.4

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault();
   console.log(loginInput.value);
 }
+function handleLinkClick() {
+  console.log(event);
+  alert("Clicked!")
+}
+
 
 loginForm.addEventListener("submit", onLoginSubmit);
-
+link.addEventListener("click", handleLinkClick);
