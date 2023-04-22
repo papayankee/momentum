32e77aed1433dbc4d0b8b7f1f17b2092797e2065
pyglee <realmanpaul@gmail.com>
2023-04-23 04:55:13 +0900

    4.4 Getting Username
    backtick

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
-}
-function handleLinkClick() {
-  console.log(event);
-  alert("Clicked!")
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = 'Hello ${username}';
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
