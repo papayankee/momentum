98c917585e1947c6314cd56a26043b1681e14065
pyglee <realmanpaul@gmail.com>
2023-04-29 04:06:41 +0900

    7.1

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,22 @@
-const toDoForm = document.querySelector("todo-form");
-const toDoInput = toDoForm.querySelector("input");
-const toDoList = document.querySelector("todo-list");
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
+
 
 function handleToDoSunmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    paintTodo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSunmit);
